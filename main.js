21789dd530f16ee5f56993576ee171084e133092
passjoe <118340470+passjoe@users.noreply.github.com>
2023-07-06 19:49:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,15 +6,25 @@ img.src = 'IMG_5283.png'; // use a picture of a cookie
 
 let score = 0;
 let scoreText = document.getElementById("score");
-let button = document.getElementById("btnClick");
 
 img.onload = function() {
-  ctx.drawImage(img, canvas.width / 2 - img.width / 2, canvas.height / 2 - img.height / 2);
+  drawCookie();
 };
 
-button.onclick = function() {
-  score++;
-  scoreText.innerHTML = "Cookies: " + score;
+canvas.addEventListener('click', function(event) {
+  let rect = canvas.getBoundingClientRect();
+  let x = event.clientX - rect.left;
+  let y = event.clientY - rect.top;
+  let cookieX = canvas.width / 2 - img.width / 2;
+  let cookieY = canvas.height / 2 - img.height / 2;
+
+  if(y > cookieY && y < cookieY + img.height && x > cookieX && x < cookieX + img.width) {
+    score++;
+    scoreText.innerHTML = "Cookies: " + score;
+  }
+});
+
+function drawCookie() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(img, canvas.width / 2 - img.width / 2, canvas.height / 2 - img.height / 2);
-};
+}
