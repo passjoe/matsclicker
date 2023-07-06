fb5d2f3a868b620a64143f0e61de05b75ee24f5b
passjoe <118340470+passjoe@users.noreply.github.com>
2023-07-06 19:22:56 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+let canvas = document.getElementById("gameCanvas");
+let ctx = canvas.getContext("2d");
+
+let img = new Image();
+img.src = 'cookie.png'; // use a picture of a cookie
+
+let score = 0;
+let scoreText = document.getElementById("score");
+let button = document.getElementById("btnClick");
+
+img.onload = function() {
+  ctx.drawImage(img, canvas.width / 2 - img.width / 2, canvas.height / 2 - img.height / 2);
+};
+
+button.onclick = function() {
+  score++;
+  scoreText.innerHTML = "Cookies: " + score;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(img, canvas.width / 2 - img.width / 2, canvas.height / 2 - img.height / 2);
+};
